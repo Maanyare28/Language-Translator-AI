08e7ed26dd2c4b662e4a12678e96febc1dc349e3
Maanyare28 <90015669+Maanyare28@users.noreply.github.com>
2025-01-07 22:10:12 -0600

    Create languageTranslator.py

---
+++ languageTranslator.py
@@ -0,0 +1,47 @@
+from googletrans import Translator
+from tkinter import *
+from tkinter import filedialog
+
+#logic
+translater = Translator()
+
+output = translater.translate("Hello World", dest="hi ")
+
+print(output)
+
+# Create a python window
+
+
+#Customize python window so it can look the way I want it to look
+
+# Have 2 sections one for the language you want to translate from and the other the lagnauge u want translated
+
+# Add a dropdown of all the languages for both
+
+#Have a translate button to translate it 
+
+#Actual Translator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Bonus: being able to take an image and then converting it to text, then after that translating that text into another language.
