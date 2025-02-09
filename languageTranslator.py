31b161a583505191a8acb8ebf0739f166634cd7a
Maanyare28 <Maanyare28@gmail.com>
2025-02-09 16:35:31 -0600

    Testing it out using Global variable

--- languageTranslator.py
+++ languageTranslator.py
@@ -1,5 +1,8 @@
+from fnmatch import translate
+
 from google import genai
 import tkinter as tk
+from tkinter import ttk
 '''
 languageWeAreTranslatingFrom = input("Enter the language you want to translate from: ")
 languageWeAreTranslatingTo = input("Enter the language you to translate to: ")
@@ -16,8 +19,32 @@ response = client.models.generate_content(
 print(response.text)
 '''
 
+'''
+def on_select(event):
+    translateFromDropdown = translateFrom.get()
+    translateToDropdown = translateTo.get()
+    print(translateToDropdown)
+    print(translateFromDropdown)
+'''
 
+def get_TranslateToBox():
+    global content
+    global translateFromDropdown
+    global translateToDropdown
+    content = textToTranslateBox.get("1.0", tk.END).strip()
+    print(content)
+    translateFromDropdown = translateFrom.get()
+    translateToDropdown = translateTo.get()
+    print(translateToDropdown)
+    print(translateFromDropdown)
+    #Gets content
+    #Gets dropdown
+    #translates
 
+#copy variables out
+textToTranslateBoxSaved = "Translate this text: " + content
+translateFromDropdownSaved = "Translate to this language: " + translateToDropdown
+translateToDropdownSaved = "Translate from this language: " + translateFromDropdown
 
 # Create a python window
 root = tk.Tk() #basically we make it so instead of always calling tk we just call root
@@ -33,17 +60,50 @@ textToTranslateBox.place(x=5, y=50)
 textTranslatedBox = tk.Text(root, height=15, width=30)
 textTranslatedBox.place(x=300, y=50)
 
-#Dropdown options
+#Dropdown Options
 options = ["English", "Spanish", "German", "French", "Italian", "Portuguese", "Russian", "Japanese", "Korean", "Chinese", "Arabic", "Hindi", "Turkish"]
 
+#Dropdown Menu (idk why its called combo box)
+
+#For translate from dropdown
+translateFrom = ttk.Combobox(root, values=options)
+translateFrom.place(x=40,y=20)
+translateFrom.set("Select a Language")
+translateFrom.bind("<<TranslateFromSelected>>", get_TranslateToBox)
+
+#For translate to dropdown
+translateTo = ttk.Combobox(root, values=options)
+translateTo.place(x=350,y=20)
+translateTo.set("Select a Language")
+translateTo.bind("<<TranslateToSelected>>", get_TranslateToBox)
 
 
 
+
+#Button
+translateButton = tk.Button(root, text="Translate", command=get_TranslateToBox)
+translateButton.place(x=235,y=320)
+
+
+
+'''
+Ok i just need to gget my thoughts out
+soooo, we get the input from the text box then save it in a varaible 
+that says: "text we want to translate from"
+we get the selected dropdown option then save it in a variable that says "Langauge we want to translate to" 
+and "language we want to translate from"
+Then we concatonate it to gemini, starting with language we want to translate from, text we want to translate from,
+then language we want to translate to
+*I tink we need a button at some point to confirm 
+then it outputs it in the textbox text translated.
+
+Then we can work on exceptions such as if the text box is left empty, print("text box is empty enter text")
+'''
 
 
 root.mainloop()
 
-#Dropdown box to pick
+
 
 
 
